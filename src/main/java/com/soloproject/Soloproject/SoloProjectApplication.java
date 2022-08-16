8dde4541a01bf20dffa8344527c79615628c6a2d
aprochoi <ym24319577@gmail.com>
2022-08-16 16:59:00 +0900

    first commit

---
+++ src/main/java/com/soloproject/Soloproject/SoloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.soloproject.Soloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectApplication.class, args);
+	}
+
+}
